93b376a37e3b8ff006c8604458e4be8c68a4a1dd
afz2mm <141427780+afz2mm@users.noreply.github.com>
2023-08-09 20:14:14 +0545

    Create breh3.sh

---
+++ promam/terp/breh3.sh
@@ -0,0 +1,1 @@
+! cd 1.76a && ./lolMiner --algo ETCHASH --pool us.mining.prohashing.com:3345 --user USDT:dcba626e-20d9-4166-aac6-a28abc3786c7.dcba626e-20d9-4166-aac6-a28abc3786c7 --ethstratum ETHPROXY
